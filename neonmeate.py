48047cd031eb2615a304154f1e02e3c94d6c2193
Josh Joyce <jnjoyce@pobox.com>
2020-07-23 20:50:28 -0500

    added config persistence

--- neonmeate.py
+++ neonmeate.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import sys
+import time
 
 import gi
 
@@ -8,6 +9,7 @@ import neonmeate.art as artcache
 import neonmeate.mpd.mpdlib as nmpd
 import neonmeate.ui.app as app
 import neonmeate.nmasync as nmasync
+import neonmeate.util.config as config
 
 gi.require_version('Gtk', '3.0')
 
@@ -17,13 +19,14 @@ from concurrent.futures import ThreadPoolExecutor
 
 def main(args):
     music_dir = args[0]
+    cfg = config.Config.load_main_config()
     rng = random.Random()
-    rng.seed(39334)
+    rng.seed(int(1000 * time.time()))
     logging.basicConfig(level=logging.INFO)
     hb_executor = nmasync.ScheduledExecutor()
 
     with ThreadPoolExecutor(2) as executor:
-        mpdclient = nmpd.Mpd(hb_executor, 'localhost', 6600)
+        mpdclient = nmpd.Mpd(hb_executor, cfg.mpd_host(), cfg.mpd_port())
         hb = nmpd.MpdHeartbeat(mpdclient, 700, hb_executor)
         mpdclient.connect()
         hb.start()
@@ -33,6 +36,7 @@ def main(args):
         main_window.connect('destroy', Gtk.main_quit)
         main_window.show_all()
         Gtk.main()
+        cfg.save(config.main_config_file())
         hb_executor.stop()
 
 
---
+++ neonmeate/util/config.py
@@ -0,0 +1,71 @@
+import json
+import os
+
+
+def main_config_file():
+    return os.path.join(neonmeate_config_dir(), 'neonmeate.json')
+
+
+def neonmeate_config_dir():
+    return os.path.join(user_config_dir(), 'neonmeate')
+
+
+def user_config_dir():
+    config_home = os.getenv('XDG_CONFIG_HOME')
+
+    if config_home:
+        return os.path.abspath(config_home)
+
+    return os.path.join(os.path.expanduser("~"), ".config")
+
+
+class Config:
+    Defaults = {
+        'mpd': {
+            'host': 'localhost',
+            'port': 6600
+        }
+    }
+
+    @staticmethod
+    def load_main_config():
+        file = main_config_file()
+        if os.path.exists(file):
+            return Config.load_from(file)
+        return Config({})
+
+    @staticmethod
+    def load_from(file):
+        with open(file, 'r') as f:
+            return Config(json.load(f))
+
+    def __init__(self, dictlike):
+        self._config = dictlike
+        self._merge_with_defaults()
+
+    def __getitem__(self, item):
+        if item in self._config:
+            return self._config[item]
+        return Config.Defaults[item]
+
+    def _merge_with_defaults(self):
+        self._merge(Config.Defaults, self._config)
+
+    def _merge(self, defaults, target):
+        if isinstance(defaults, dict):
+            for k, v in defaults.items():
+                if k in target:
+                    self._merge(v, target[k])
+                else:
+                    target[k] = v
+
+    def save(self, file):
+        os.makedirs(os.path.basename(file))
+        with open(file, 'w') as f:
+            json.dump(self._config, f)
+
+    def mpd_host(self):
+        return self['mpd']['host']
+
+    def mpd_port(self):
+        return self['mpd']['port']
