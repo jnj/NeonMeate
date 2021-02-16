69a20a132d4a369c778fcd11a398e569f687948f
Josh Joyce <jnjoyce@pobox.com>
2021-02-16 10:19:20 -0600

    make executable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='neonmeate',
+    version='1.0',
+    packages=['neonmeate', 'neonmeate.ui', 'neonmeate.mpd', 'neonmeate.util'],
+    url='https://github.com/jnj/NeonMeate/',
+    license='',
+    author='josh',
+    author_email='jnjoyce@pobox.com',
+    description='A graphical client for mpd'
+)
